ed13099ad53fdee257b06221f41918f6006e33a5
ANNA TSANG <TSANGAN@OOCL.COM>
2021-12-13 09:45:15 +0800

    [counter][Anna] feat: undate after code review

--- counter.js
+++ counter.js
@@ -13,7 +13,6 @@ function doMinus(position){
 function doCalculation(){
     var number1 = document.getElementById("first").innerHTML;
     var number2 = document.getElementById("second").innerHTML;
-    var answer = document.getElementById("answer").innerHTML;
     answer = parseInt(number1) + parseInt(number2);
     document.getElementById("answer").innerHTML = answer;
 }
